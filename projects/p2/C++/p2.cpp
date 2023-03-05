22cb8f4b5a5415987885b7421d9a7b162f0b2d5c
--show-origin <karthik.k.jw@gmail.com>
2023-03-04 22:03:19 -0500

    Optimization 0

--- projects/p2/C++/p2.cpp
+++ projects/p2/C++/p2.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include<iostream>
 
 #include "llvm-c/Core.h"
 
@@ -162,7 +163,112 @@ static llvm::Statistic CSELdElim = {"", "CSELdElim", "CSE redundant loads"};
 static llvm::Statistic CSEStore2Load = {"", "CSEStore2Load", "CSE forwarded store to load"};
 static llvm::Statistic CSEStElim = {"", "CSEStElim", "CSE redundant stores"};
 
-static void CommonSubexpressionElimination(Module *) {
+bool isDead(Instruction &I) {
+
+  int opcode = I.getOpcode();
+  switch(opcode){
+  case Instruction::Add:
+  case Instruction::FNeg:
+  case Instruction::FAdd: 	
+  case Instruction::Sub:
+  case Instruction::FSub: 	
+  case Instruction::Mul:
+  case Instruction::FMul: 	
+  case Instruction::UDiv:	
+  case Instruction::SDiv:	
+  case Instruction::FDiv:	
+  case Instruction::URem: 	
+  case Instruction::SRem: 	
+  case Instruction::FRem: 	
+  case Instruction::Shl: 	
+  case Instruction::LShr: 	
+  case Instruction::AShr: 	
+  case Instruction::And: 	
+  case Instruction::Or: 	
+  case Instruction::Xor: 	
+  case Instruction::Alloca:
+  case Instruction::GetElementPtr: 	
+  case Instruction::Trunc: 	
+  case Instruction::ZExt: 	
+  case Instruction::SExt: 	
+  case Instruction::FPToUI: 	
+  case Instruction::FPToSI: 	
+  case Instruction::UIToFP: 	
+  case Instruction::SIToFP: 	
+  case Instruction::FPTrunc: 	
+  case Instruction::FPExt: 	
+  case Instruction::PtrToInt: 	
+  case Instruction::IntToPtr: 	
+  case Instruction::BitCast: 	
+  case Instruction::AddrSpaceCast: 	
+  case Instruction::ICmp: 	
+  case Instruction::FCmp: 	
+  case Instruction::PHI: 
+  case Instruction::Select: 
+  case Instruction::ExtractElement: 	
+  case Instruction::InsertElement: 	
+  case Instruction::ShuffleVector: 	
+  case Instruction::ExtractValue: 	
+  case Instruction::InsertValue: 
+    if ( I.use_begin() == I.use_end() )
+         {
+	       return true;
+         }
+         break;
+
+  case Instruction::Load:
+    {
+      LoadInst *li = dyn_cast<LoadInst>(&I);
+      if (li && li->isVolatile())
+	   return false;
+      if (I.use_begin() == I.use_end())
+         return true;
+      break;
+      
+    }
+  
+  default: 
+    // any other opcode fails 
+      return false;
+  }
+
+  
+  return false;
+}
+
+static void CommonSubexpressionElimination(Module *M) 
+{
     // Implement this function
+
+    // optimization 0
+      if (M!=nullptr)
+      {
+        for (auto func = M->begin();func!=M->end();func++)
+        {
+            for (auto bb = func->begin();bb!=func->end();bb++)
+            {
+                for (auto instr = bb->begin();instr!=bb->end();)
+                {
+                    instr->print(errs(),true);
+                    if (isDead(*instr))
+                    {
+                        //std::cout<<" is dead.\n";
+                        instr = instr->eraseFromParent();
+                    }
+                    else instr++;
+                }
+
+                for (auto instr = bb->begin();instr!=bb->end();instr++)
+                {
+                    Value *val = SimplifyInstruction(&*instr,M->getDataLayout());
+                    if (val)
+                    {
+                        errs() << val<<"\n";
+                    }
+
+                }
+            }
+        }
+      }
 }
 
